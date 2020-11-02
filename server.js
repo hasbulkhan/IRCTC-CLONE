9363592e7b89703490ae62a1416ed2badc99b4bf
hasbulkhan <43811526+hasbulkhan@users.noreply.github.com>
2020-11-02 20:39:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+var express=require('express');
+var app=express();
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+'C:/Users/HASBUL KHAN/Desktop/Indian-Railway-Reservation-Clone-master/Indian-Railway-Reservation-Clone-master/rA/ABOUT INDIAN RAILWAYS.html'
+});
+app.listen(127.0.0.1.8080);
+
+
